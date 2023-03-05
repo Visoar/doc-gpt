cddca0d6c8eae061fde771b5b4f25afa2efc3917
Visoar <visoarcn@gmail.com>
2023-03-05 19:40:47 +0800

    fork

--- doc-gpt.py
+++ doc-gpt.py
@@ -2,6 +2,8 @@ import os
 import openai
 import pypdf
 import streamlit as st
+from streamlit_chat import message
+
 from langchain.llms import OpenAIChat
 from langchain.vectorstores import FAISS
 from langchain.chains import VectorDBQA
@@ -14,7 +16,6 @@ def split_pdf(fpath,chunk_chars=4000,overlap=50):
     Pre-process PDF into chunks
     Some code from: https://github.com/whitead/paper-qa/blob/main/paperqa/readers.py
     """
-    st.info("`Reading and splitting doc ...`")
     pdfReader = pypdf.PdfReader(fpath)
     splits = []
     split = ""
@@ -25,6 +26,8 @@ def split_pdf(fpath,chunk_chars=4000,overlap=50):
         if len(split) > chunk_chars:
             splits.append(split[:chunk_chars])
             split = split[chunk_chars - overlap:]
+        if len(split) <= chunk_chars:
+            splits.append(split)
     return splits
 
 @st.cache_resource
@@ -32,37 +35,45 @@ def create_ix(splits):
     """ 
     Create vector DB index of PDF
     """
-    st.info("`Building index ...`")
     embeddings = OpenAIEmbeddings()
     docsearch = FAISS.from_texts(splits,embeddings)
     return docsearch
 
-# Auth
-st.sidebar.image("Img/reading.jpg")
-api_key = st.sidebar.text_input("`OpenAI API Key:`", type="password")
-st.sidebar.write("`By:` [@RLanceMartin](https://twitter.com/RLanceMartin)")
+st.sidebar.header("PDF 阅读助手")
+# st.sidebar.image("Img/reading.jpg")
+# Auth & Setting
+api_key = st.sidebar.text_input("OpenAI API Key:", type="password")
+
 os.environ["OPENAI_API_KEY"] = api_key
-chunk_chars = st.sidebar.radio("`Choose chunk size for splitting`", (2000, 3000, 4000), index=1)
-st.sidebar.info("`Larger chunk size can produce better answers, but may high ChatGPT context limit (4096 tokens)`")
+chunk_chars = st.sidebar.radio("选择要拆分的「块」大小", (3000, 3500, 4000), index=1)
+langInfo = st.sidebar.radio("选择期望回复所用语言",("使用中文回复","Reply in English"))
+st.sidebar.info("`更大的「块」大小可以产生更好的答案，但可能会超过ChatGPT上下文限制（4096个tokens）。`")
 
 # App 
-st.header("`doc-gpt`")
-st.info("`Hello! I am a ChatGPT connected to whatever document you upload.`")
-uploaded_file_pdf = st.file_uploader("`Upload PDF File:` ", type = ['pdf'] , accept_multiple_files=False)
+# st.write("你好！我是 ChatGPT，请上传你要解读的 PDF 文档。")
+uploaded_file_pdf = st.file_uploader("上传 PDF 文件: ", type = ['pdf'] , accept_multiple_files=False)
 if uploaded_file_pdf and api_key:
     # Split and create index
-    d=split_pdf(uploaded_file_pdf,chunk_chars)
-    ix=create_ix(d)
+    with st.spinner("文档读取和拆分中..."):
+        d=split_pdf(uploaded_file_pdf,chunk_chars)
+    with st.spinner("索引创建中..."):
+        ix=create_ix(d)
     # Use ChatGPT with index QA chain
     llm = OpenAIChat(temperature=0)
     chain = VectorDBQA.from_chain_type(llm, chain_type="stuff", vectorstore=ix)
-    query = st.text_input("`Please ask a question:` ","What is this document about?")
+    query = st.text_input("请输入问题后按回车： ","总结这个文件的主要内容。")
+    message(query, is_user=True) 
+    query = query + "。" + langInfo
+    print(query)
     try:
-        st.info("`%s`"%chain.run(query))
+        with st.spinner("思考中..."):
+            reply = chain.run(query)
+            # st.info(reply)
+            message(reply)
     except openai.error.InvalidRequestError:
         # Limitation w/ ChatGPT: 4096 token context length
         # https://github.com/acheong08/ChatGPT/discussions/649
-        st.warning('Error with model request, often due to context length. Try reducing chunk size.', icon="⚠️")
+        st.warning('模型请求出错，通常是由于上下文长度问题。尝试减少「块」的大小。', icon="⚠️")
 
 else:
-    st.info("`Please enter OpenAI Key and upload pdf file`")
+    st.info("请输入 OpenAI Key 并上传 PDF 文件")
